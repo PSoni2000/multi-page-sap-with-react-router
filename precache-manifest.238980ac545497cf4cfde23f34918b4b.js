8ddcce6b0700e7f33fd0b64397abc46ce3f49c8d
Pulkit Soni <pulkitsoni2000@gmail.com>
2023-08-27 23:35:28 +0530

    Updates

---
+++ precache-manifest.238980ac545497cf4cfde23f34918b4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab92f29c5d3107785fd5",
+    "url": "/multi-page-sap-with-react-router/static/css/main.e1343a16.chunk.css"
+  },
+  {
+    "revision": "ab92f29c5d3107785fd5",
+    "url": "/multi-page-sap-with-react-router/static/js/main.ab92f29c.chunk.js"
+  },
+  {
+    "revision": "b202368b2ddeccc66314",
+    "url": "/multi-page-sap-with-react-router/static/js/1.b202368b.chunk.js"
+  },
+  {
+    "revision": "15e7c22872c0c054ad9b",
+    "url": "/multi-page-sap-with-react-router/static/js/runtime~main.15e7c228.js"
+  },
+  {
+    "revision": "d423a15a393942f5be58044d076a7d11",
+    "url": "/multi-page-sap-with-react-router/index.html"
+  }
+];
